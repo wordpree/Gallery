e1e586132521f06a5cc67d881eaa0ce5d4ffe80a
Hai <wordpree.dev@gmail.com>
2019-08-17 12:13:33 +1000

    sider for lightbox completed

--- src/components/gridlist/GridLists.js
+++ src/components/gridlist/GridLists.js
@@ -1,16 +1,72 @@
 import React, { useState } from "react";
 import { useStateValue } from "../ApiDataProvider";
 import Loading from "../Loading";
-import { GridList, GridListTile } from "@material-ui/core";
+import {
+  Fab,
+  GridList,
+  GridListTile,
+  Dialog,
+  Card,
+  CardMedia
+} from "@material-ui/core";
+import { ArrowLeft, ArrowRight } from "mdi-material-ui";
 import useQueryMedia from "@material-ui/core/useMediaQuery";
-import GridListsDialog from "./GridListsDialog";
 import PrevPageFab from "../PrevPageFab";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: "rgba(0, 0, 0, 0.6)"
+  },
+  sliders: {
+    height: "80vh"
+  },
+  slide: {
+    opacity: 0,
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+    transition: "opacity 0.5s ease-in-out"
+  },
+  card: {
+    width: "100%",
+    height: "100%"
+  },
+  media: {
+    width: "100%",
+    height: "100%"
+  },
+  current: {
+    opacity: 1
+  },
+
+  iconBtn: {
+    position: "absolute",
+    top: "50%",
+    transform: "translateY(-50%)",
+    background: "rgba(0,0,0,0.4)",
+    zIndex: 999,
+    color: "#fff",
+    "&:hover": {
+      background: "#f73859"
+    }
+  },
+  left: {
+    left: "0%"
+  },
+  right: {
+    right: "0%"
+  }
+});
 
 export default function GridLists() {
+  const classes = useStyles();
   const state = useStateValue();
   const md = useQueryMedia("(min-width:760px)");
   const lg = useQueryMedia("(min-width:1025px)");
-  const [ele, setEle] = useState({ open: false, index: 0 });
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+
   const i = { acc: 0, cols: 0 };
   let cellHeight = lg ? 335 : md ? 250 : 180;
 
@@ -39,7 +95,8 @@ export default function GridLists() {
               key={index}
               cols={cols}
               onClick={() => {
-                setEle({ open: true, index: index });
+                setOpen(true);
+                setIndex(index);
               }}
             >
               <img src={item.url} alt={item.title} />
@@ -47,11 +104,55 @@ export default function GridLists() {
           );
         })}
       </GridList>
-      <GridListsDialog
-        open={ele.open}
-        close={() => setEle({ open: false, index: ele.index })}
-        index={ele.index}
-      />
+      <Dialog
+        fullWidth
+        maxWidth="xl"
+        classes={{ container: classes.container }}
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div className={classes.sliders}>
+          {state.gallery.map((item, key) => (
+            <div
+              className={classes.slide}
+              key={key}
+              style={{ opacity: key === index ? 1 : 0 }}
+            >
+              <Card className={classes.card}>
+                <Fab
+                  className={`${classes.iconBtn} ${classes.left}`}
+                  onClick={() =>
+                    setIndex(prevIndex => {
+                      prevIndex =
+                        prevIndex === 0
+                          ? state.gallery.length - 1
+                          : prevIndex - 1;
+                      return prevIndex;
+                    })
+                  }
+                >
+                  <ArrowLeft />
+                </Fab>
+                <Fab
+                  className={`${classes.iconBtn} ${classes.right}`}
+                  onClick={() =>
+                    setIndex(prevIndex => {
+                      prevIndex =
+                        prevIndex === state.gallery.length - 1
+                          ? 0
+                          : prevIndex + 1;
+                      return prevIndex;
+                    })
+                  }
+                >
+                  <ArrowRight />
+                </Fab>
+                <CardMedia image={item.urlHd} className={classes.media} />
+              </Card>
+            </div>
+          ))}
+        </div>
+      </Dialog>
       <PrevPageFab to="/" />
     </>
   );
